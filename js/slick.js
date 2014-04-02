f0d3e9cac8f0d9a8ecfda7d789f12a7a2de89549
Pestana Gonçalo <g6pestana@gmail.com>
2014-04-02 15:59:20 +0300

    refacoring

--- js/slick.js
+++ js/slick.js
@@ -9,66 +9,108 @@
  		//Is this cache system working ? Test!
  		var allEntriesMetadata_cache;
 
- 		function _getAllEntriesGithub(user, repo, callback) {
- 			data = [];
- 			getResourceGithub(user+"/"+repo+"/contents", 
- 				function(rawData) {  		
- 					for(var i = 0; i<rawData.length; i++) {
- 						var newEntry = new Object();
- 						metadata = rawData[i].name.split("@");
- 						data.push([metadata[0], metadata[1]]);
- 					}
- 					callback(data);
- 				});
+ 		function getGithubUser() {
+ 			return this.user;
+ 		}
+ 		
+ 		function getGithubRepo() {
+ 			return this.gitRepo;
+ 		}
+ 		
+ 		function getAllEntriesMetadata() {
+ 			console.log(this.repo);
+ 			return allEntriesMetadata_cache; 
  		}
 
- 		function _getSingleEntryGithub(user, repo, entry_name, callback) {
- 			getResourceGithub(user+"/"+repo+"/contents/"+entry_name, 
- 				function(rawData) {
- 					metadata = rawData.name.split("@");
- 					content = decodeContent(rawData.content);
- 					data = [[metadata[0], metadata[1], content]];
- 						
- 					callback(data);
- 				}); 
+ 		function setAllEntriesMetadata(data) {
+ 			this.allEntriesMetadata_cache = data;
  		}
 
+ 		/*New organization*/
+ 		function setEnv() {
+ 			gitUser = arguments[0];
+ 			gitRepo = arguments[1];
+ 			plugins = Array.prototype.slice.call(arguments, 2);
 
- 		function setGithubConfigs(user, repo) {
- 			this.user = user;
- 			this.repo = repo;			
- 		}
+ 			this.user = gitUser;
+ 			this.repo = gitRepo;
 
- 		function fetchAllEntriesMetadata(appendHandler, templatePath) {
- 			if (typeof(this.allEntriesMetadata_cache) == 'undefined') {
- 				_getAllEntriesGithub(this.user,this.repo, function(data) {
- 					this.allEntriesMetadata_cache = data;
- 					fetchTemplate(templatePath, function(wrapper) {
- 						addToDOM(data, appendHandler, wrapper);
- 					});
- 			});	
- 			} else {
+ 			_getAllEntriesMetadataToCache(gitUser, gitRepo, function() {
+				//trigger async function executer
+				_asyncExecuter(plugins);
+			});
+ 		};
+
+ 		function _asyncExecuter() {
+ 			pluginsData = arguments[0];
+ 			for(var i=0; i<pluginsData.length; i++) {
+				//call funcs with params
+				console.log(pluginsData[i]);
+				pluginsData[0]['plugin'](pluginsData.shift());
+			}}
+
+
+			function _getAllEntriesMetadataToCache(user, repo, callback) {
+				data = [];
+				getResourceGithub(user+"/"+repo+"/contents", 
+					function(rawData) {  		
+						for(var i = 0; i<rawData.length; i++) {
+							var newEntry = new Object();
+							metadata = rawData[i].name.split("@");
+							data.push([metadata[0], metadata[1]]);
+						}
+						this.allEntriesMetadata_cache = data;
+						console.log(Slick.setAllEntriesMetadata(allEntriesMetadata_cache));
+						callback();
+					});	
+			};
+
+
+			function _getSingleEntryGithub(user, repo, entry_name, callback) {
+				getResourceGithub(user+"/"+repo+"/contents/"+entry_name, 
+					function(rawData) {
+						metadata = rawData.name.split("@");
+						content = decodeContent(rawData.content);
+						data = [[metadata[0], metadata[1], content]];
+
+						callback(data);
+					}); 
+			}
+
+
+			return {
+				getGithubUser: getGithubUser,
+				getGithubRepo: getGithubRepo,
+				getAllEntriesMetadata: getAllEntriesMetadata,
+				setAllEntriesMetadata: setAllEntriesMetadata,
+				setEnv: setEnv
+			}
+
+		})();
+
+
+		/*Native Plugins*/
+
+		function fetchAllEntriesMetadata() {
+			
+			appendHandler = arguments[0]['appendHandler'];
+			templatePath = arguments[0]['templatePath'];
+
+			console.log(Slick.getAllEntriesMetadata());
+
+			fetchTemplate(templatePath, function(wrapper) {
+				addToDOM(Slick.getAllEntriesMetadata(), appendHandler, wrapper);
+			});
+		}
+
+		function fetchSingleEntry(entry_name, appendHandler, templatePath) {
+ 			//get Next and Previous post
+ 			_getSingleEntryGithub(this.user, this.repo, entry_name, function(data) {
  				fetchTemplate(templatePath, function(wrapper) {
- 					addToDOM(data, appendHandler, wrapper);
- 			});
+ 					addToDOM([data], appendHandler, wrapper);
+ 				});
+ 			});	
  		}
- 	}
-
- 	function fetchSingleEntry(entry_name, appendHandler, templatePath) {
- 		_getSingleEntryGithub(this.user, this.repo, entry_name, function(data) {
- 			fetchTemplate(templatePath, function(wrapper) {
- 				addToDOM([data], appendHandler, wrapper);
- 			});
- 		});	
- 	}
-
- 	return {
- 		setGithubConfigs: setGithubConfigs,
- 		fetchSingleEntry: fetchSingleEntry,
- 		fetchAllEntriesMetadata: fetchAllEntriesMetadata
- 	}
-
- })();
 
 
 /*
